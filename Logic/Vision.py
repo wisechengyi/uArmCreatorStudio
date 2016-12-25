2b74fdf10750c8b7b51f4580177fdc5ae9d28bba
wisechengyi <wisechengyi@gmail.com>
2016-12-25 00:46:03 -0800

    detect rectangles

--- Logic/Vision.py
+++ Logic/Vision.py
@@ -757,6 +757,34 @@ class PlaneTracker(Tracker):
         # Apply the normal mask to the frame
         frame[mask > 0] = mask[mask > 0]   #  *= mask == 0
 
+        def find_squares(img):
+
+            def angle_cos(p0, p1, p2):
+                d1, d2 = (p0 - p1).astype('float'), (p2 - p1).astype('float')
+                return abs(np.dot(d1, d2) / np.sqrt(np.dot(d1, d1) * np.dot(d2, d2)))
+
+            img = cv2.GaussianBlur(img, (5, 5), 0)
+            squares = []
+            for gray in cv2.split(img):
+                for thrs in range(0, 255, 26):
+                    if thrs == 0:
+                        bin = cv2.Canny(gray, 0, 50, apertureSize=5)
+                        bin = cv2.dilate(bin, None)
+                    else:
+                        retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
+                    bin, contours, hierarchy = cv2.findContours(bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                    for cnt in contours:
+                        cnt_len = cv2.arcLength(cnt, True)
+                        cnt = cv2.approxPolyDP(cnt, 0.02 * cnt_len, True)
+                        if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+                            cnt = cnt.reshape(-1, 2)
+                            max_cos = np.max([angle_cos(cnt[i], cnt[(i + 1) % 4], cnt[(i + 2) % 4]) for i in range(4)])
+                            if max_cos < 0.1:
+                                squares.append(cnt)
+            return squares
+
+        squares = find_squares(frame)
+        cv2.drawContours(frame, squares, -1, (0, 255, 0), 3)
 
         return frame
 
