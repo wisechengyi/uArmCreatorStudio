4a57d7653bb87d65faaa5ad9fe3009baeb027c4f
wisechengyi <wisechengyi@gmail.com>
2016-12-30 23:48:30 -0800

    square

--- Logic/Vision.py
+++ Logic/Vision.py
@@ -29,6 +29,9 @@ import os
 import cv2
 import numpy as np
 import math
+
+import sys
+
 from Logic.Global import printf
 from collections  import namedtuple
 from time         import sleep
@@ -756,6 +759,42 @@ class PlaneTracker(Tracker):
         # Apply the normal mask to the frame
         frame[mask > 0] = mask[mask > 0]   #  *= mask == 0
 
+        def find_squares(img):
+            def angle_cos(p0, p1, p2):
+                d1, d2 = (p0 - p1).astype('float'), (p2 - p1).astype('float')
+                return abs(np.dot(d1, d2) / np.sqrt(np.dot(d1, d1) * np.dot(d2, d2)))
+
+            img = cv2.GaussianBlur(img, (5, 5), 0)
+            squares = []
+            angles = []
+            min_area = sys.maxsize
+            for gray in cv2.split(img):
+                for thrs in range(0, 255, 26):
+                    if thrs == 0:
+                        bin = cv2.Canny(gray, 0, 50, apertureSize=5)
+                        bin = cv2.dilate(bin, None)
+                    else:
+                        retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
+                    bin, contours, hierarchy = cv2.findContours(bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                    for cnt in contours:
+                        cnt_len = cv2.arcLength(cnt, True)
+                        cnt = cv2.approxPolyDP(cnt, 0.02 * cnt_len, True)
+                        if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+                            cnt = cnt.reshape(-1, 2)
+                            max_cos = np.max([angle_cos(cnt[i], cnt[(i + 1) % 4], cnt[(i + 2) % 4]) for i in range(4)])
+                            if max_cos < 0.1:
+                                (x, y), (w, h), theta = cv2.minAreaRect(cnt)
+                                area = w * h
+                                if area < min_area:
+                                    min_area = area
+                                    squares = [cnt]
+                                    angles = [theta]
+            return squares, angles
+
+        squares, angles = find_squares(frame)
+        if squares:
+            print("angle: {}".format(angles))
+            cv2.drawContours(frame, squares, -1, (0, 255, 0), 3)
 
         return frame
 
